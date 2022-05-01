05ab1d5b6b6eb13f64a6d3cd1fec9197fd2bc3f7
gavin wun <gavinwun@live.com>
2022-05-01 16:37:24 +1200

    update: progress

--- blockchain.py
+++ blockchain.py
@@ -12,6 +12,19 @@ participants = {'Gavin'}
 def hash_block(last_block):
     return '-'.join([str(last_block[key]) for key in last_block])
 
+def get_balance(participant):
+    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+    amount_sent = 0
+    for tx in tx_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in tx_recipient:
+        if len(tx) > 0:
+            amount_received += tx[0]
+    return amount_received - amount_sent
+
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain. """
     if len(blockchain) < 1:
@@ -43,6 +56,7 @@ def mine_block():
         'transactions': open_transactions
     }
     blockchain.append(block)
+    return True
 
 def get_transaction_value():
     """ Returns the input of the user as float """
@@ -89,7 +103,8 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         print_blockchain_element()
     elif user_choice == '4':
@@ -109,6 +124,7 @@ while waiting_for_input:
         print_blockchain_element()
         print('Invalid blockchain!')
         break
+    print(get_balance('Gavin'))
 else:
     print('User left!')    
 
