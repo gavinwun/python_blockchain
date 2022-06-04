b3dbe636d7def3d6b49b7af0a898b3c072fade19
gavin wun <gavinwun@live.com>
2022-06-05 08:56:08 +1200

    update: add pickling (commented out for now)

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,7 @@ from functools import reduce
 import hashlib as hl
 from collections import OrderedDict
 import json
+import pickle
 
 from hash_util import hash_string_256, hash_block
 
@@ -21,9 +22,14 @@ participants = {'Gavin'}
 
 def load_data():
     with open('blockchain.txt', mode='r') as f:
+        # file_content = pickle.loads(f.read())
         file_content = f.readlines()
+
         global blockchain
         global open_transactions
+
+        # blockchain = file_content['chain']
+        # open_transactions = file_content['ot']
         blockchain = json.loads(file_content[0][:-1])
         updated_blockchain = []
         for block in blockchain:
@@ -51,6 +57,11 @@ def save_data():
         f.write(json.dumps(blockchain))
         f.write('\n')
         f.write(json.dumps(open_transactions))
+        # save_data = {
+        #     'chain': blockchain,
+        #     'ot': open_transactions
+        # }
+        # f.write(pickle.dumps(save_data))
 
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
