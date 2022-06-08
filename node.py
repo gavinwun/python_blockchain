1b4c9c5a7b91859e0a9fe445a26ff0732e60937e
gavin wun <gavinwun@live.com>
2022-06-08 17:25:27 +1200

    update: add initial UI

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from itsdangerous import json
 
@@ -9,6 +9,10 @@ app = Flask(__name__)
 wallet = Wallet()
 blockchain = Blockchain(wallet.public_key)
 CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_ui():
+    return send_from_directory('ui', 'node.html')
 
 @app.route('/wallet', methods=['POST'])
 def create_keys():
@@ -60,10 +64,6 @@ def get_balance():
             'wallet_set_up': wallet.public_key != None
         }
         return jsonify(response), 500
-
-@app.route('/', methods=['GET'])
-def get_ui():
-    return 'This works!'
 
 @app.route('/transaction', methods=['POST'])
 def add_transaction():
